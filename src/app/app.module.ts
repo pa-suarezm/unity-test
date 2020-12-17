a39ec4a18996aed8516cadeb6605673c885bf28b
pa-suarezm <pabloasuarezm@hotmail.com>
2020-12-17 18:41:40 -0500

    stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UnityComponent } from './unity/unity.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UnityComponent
   ],
   imports: [
     BrowserModule
